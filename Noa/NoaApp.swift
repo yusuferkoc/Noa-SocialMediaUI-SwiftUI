60c63e57cbc77b8c2469ffb0964b854da7117585
yusuf <yusuferkoc99@gmail.com>
2023-12-05 15:15:01 +0300

    Initial Commit

---
+++ Noa/NoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoaApp.swift
+//  Noa
+//
+//  Created by Yusuf Erkoç on 5.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
